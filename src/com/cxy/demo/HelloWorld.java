b7565b9bd5304ef0bb07fd400eb83cd7c126bbf3
caixueyuan <2529982859@qq.com>
2017-07-09 14:13:52 +0800

    Initial commit

---
+++ src/com/cxy/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.cxy.demo;
+
+/**
+ * Created by 25299 on 2017/7/9.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
