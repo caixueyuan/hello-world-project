b4e950e32ca6cfb3c501412606dfe613dcd38e0f
caixueyuan <2529982859@qq.com>
2017-07-09 14:32:55 +0800

    add a line to the HelloWorld.java

--- src/com/cxy/demo/HelloWorld.java
+++ src/com/cxy/demo/HelloWorld.java
@@ -6,5 +6,7 @@ package com.cxy.demo;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world");
+        //now we add a line
+        System.out.println("hello world in another line");
     }
 }
