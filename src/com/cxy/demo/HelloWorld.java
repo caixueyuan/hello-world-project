79df2ad9b9d8c0789bec29c1985aa7baee33529e
caixueyuan <2529982859@qq.com>
2017-07-09 15:04:50 +0800

    add second line to the HelloWorld.java

--- src/com/cxy/demo/HelloWorld.java
+++ src/com/cxy/demo/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.cxy.demo;
 
-/**
+/** this is the hello-world code
  * Created by 25299 on 2017/7/9.
  */
 public class HelloWorld {
@@ -8,5 +8,7 @@ public class HelloWorld {
         System.out.println("Hello world");
         //now we add a line
         System.out.println("hello world in another line");
+        //add two line
+        System.out.println("add the second line in the file");
     }
 }
